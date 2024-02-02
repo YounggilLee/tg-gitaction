7afb9452a17280b826c848ecd95dfa5b96e832e6
gil <iamleeyounggil@gmail.com>
2024-02-02 10:16:02 -0500

    add git workflows

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/gitaction', (req, res) => {
-    res.send('Hey Younggil. It is working!')
+    res.send('Hey YO. It is working!')
 })
 
 app.listen(5050, () => {
